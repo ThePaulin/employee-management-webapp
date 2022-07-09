706900f4b04b40af521036bcbd1b0b44c4a27d54
ThePaulin <kubwirapaulin@gmail.com>
2022-07-09 11:17:47 -0400

    update UI and add input validation on login form

--- employee-mngt/src/components/Footer.js
+++ employee-mngt/src/components/Footer.js
@@ -2,12 +2,37 @@ import React from 'react'
 
 const Footer = () => {
     const currentYear = new Date().getFullYear();
+
+    function whileInView(){
+      var elements;
+      var windowHeight;
+
+      function init(){
+        elements = document.querySelectorAll('.footerText');
+        console.log(elements);
+        windowHeight = window.innerHeight;
+      }
+      function checkPosition(){
+        for(var i = 0; i < elements.length; i++){
+          var element = elements[i];
+          var positionFromTop = elements[i].getBoundingClientRect().top;
+
+          if (positionFromTop - windowHeight <= 0){
+            element.classList.add('whileInView');
+            
+          }
+        }
+      }
+      window.addEventListener('scroll', checkPosition);
+      window.addEventListener('resize', init)
+    }
+    whileInView();
    
   return (
     <div className='footer'>
-        <footer>
-            <p>Copyright AGM Inc. {currentYear}</p>
-        </footer>
+       
+      <p className='footerText'>Copyright Jumpstart Inc. {currentYear}</p>
+       
     </div>
   )
 }
